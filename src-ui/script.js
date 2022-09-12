0ee5dfe477b969465a9e7869ab46a919a9951c55
Pavel-chemist <pavel.chemist@gmail.com>
2022-09-12 09:55:18 +0400

    invoke rust fn from js

--- src-ui/script.js
+++ src-ui/script.js
@@ -1,13 +1,25 @@
 "use strict"
+const { invoke } = window.__TAURI__.tauri;
 
-let buttonElement = document.getElementById("btn");
-let rectangleElement = document.getElementById("colored-rectangle");
+const buttonElement = document.getElementById("btn");
+const rectangleElement = document.getElementById("colored-rectangle");
+const text = document.getElementById("text-output");
+
+function greet(color) {
+  invoke('greet', { name: 'Pavel', color: color })
+  // `invoke` returns a Promise
+  .then((response) => {
+    text.innerText = response;
+  });
+}
 
 buttonElement.addEventListener('click', () => {
   
   if (rectangleElement.classList.contains("green")) {
     rectangleElement.classList.remove("green");
+    greet("yellow");
   } else {
     rectangleElement.classList.add("green");
+    greet("green");
   }
 });
